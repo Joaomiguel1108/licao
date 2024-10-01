40396ef8cd9cf5aa4d7c8500f399bcdb0d79d36c
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-09-30 21:24:40 -0300

    TABUADA.py

---
+++ TABUADA.py
@@ -0,0 +1,20 @@
+#Solicite ao usuário que digite um número e imprima a tabuada desse número
+#até 10
+
+
+numero = int(input("Digite um numero"))
+
+print (f" a tabuada de {numero} é ")
+
+
+print (numero * 2) 
+print (numero * 3)
+print (numero * 4)
+print (numero * 5)
+print (numero * 6)
+print (numero * 7)
+print (numero * 8)
+print (numero * 9)
+print (numero * 10)
+
+
