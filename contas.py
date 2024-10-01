e14e975dd3d1c167a18d9a8c3b7d25107adcfb48
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-09-30 21:52:58 -0300

    Create contas.py

---
+++ contas.py
@@ -0,0 +1,15 @@
+#Solicite ao usuário que digite dois números e imprima a soma, a diferença, o
+#produto e o quociente deles.
+
+
+numero1 = float(input("digite um numero"))
+numero2 = float(input("digite outro numero"))
+
+print(f"a soma de {numero1} e {numero2} é")
+print(numero1+numero2)
+print(f"a diferença de {numero1} e {numero2} é")
+print(numero1/numero2)
+print(f"o produto de {numero1} e {numero2} é")
+print(numero1*numero2)
+print(f"a quociente de {numero1} e {numero2} é")
+print(numero1/numero2)
