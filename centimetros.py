08d8f43250148ad0b3247d010a94eab02fab84b0
Joaomiguel1108 <joaomiguelmorais1108@gmail.com>
2024-09-30 21:49:58 -0300

    Update centimetros.py

--- centimetros.py
+++ centimetros.py
@@ -1,5 +1,11 @@
+#Escreva um programa que leia um valor em metros e o exiba convertido
+#em centímetros e milímetros
+
 metros = float(input("digite um valor em metros"))
 
-centimetros = metros * 100
+c = metros * 100
 
-print(f"seu valor em metros equivale á {centimetros} centimetros")
+m = metros * 1000
+
+print(f"seu valor em metros equivale á {c} centimetros e vale {m} milimetros")
+
